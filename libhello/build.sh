d18bda27c01b6ba3fc6ea8c383d5f3a798436db9
Povilas Balciunas <balciunas90@gmail.com>
2019-07-05 13:39:26 +0300

    Fix rust cross compilation

--- libhello/build.sh
+++ libhello/build.sh
@@ -1,1 +1,6 @@
+# This is a temporary hack. For some reasons building backtrace-sys fails
+# to locate C compiler. My guess is that this is related to `cc` crate which
+# compiles C source code. In addition `cc` does compiler discovery.
+export PATH=/opt/android-ndk/arm/bin:$PATH
+
 cargo build --target=armv7-linux-androideabi
